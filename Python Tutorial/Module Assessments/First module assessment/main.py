cb08928ebcaebd9ea70b3d98ed998e082a85fec8
Francisrosqueta <39253219+Francisrosqueta@users.noreply.github.com>
2024-06-04 17:25:25 -0400

    Add files via upload
    Calculator

---
+++ Python Tutorial/Module Assessments/First module assessment/main.py
@@ -0,0 +1,19 @@
+first_number = input("Enter then 1st number:")
+
+print("The 1st number is:", first_number)
+
+second_number = input("Enter the 2nd number:")
+
+print("The 2nd number is:", second_number)
+
+sum = int(first_number) + int(second_number)
+subraction = int(first_number) - int(second_number)
+multiplication = int(first_number) * int(second_number)
+division = int(first_number) / int(second_number)
+
+
+print("The sum of the two numbers:", sum)
+print("The difference of the two numbers:", subraction)
+print("The product of the two numbers:", multiplication)
+print("The quotient of the two numbers:", division)
+
