ec26219f65bc8687ed6ba65ba82df75cfde924f6
Francisrosqueta <39253219+Francisrosqueta@users.noreply.github.com>
2024-06-04 17:02:07 -0400

    Add files via upload
    Calculate Age

---
+++ Python Tutorial/main.py
@@ -0,0 +1,11 @@
+start_year = input("Enter current year:")
+
+print("The current year now is:", start_year)
+
+end_year = input("Enter the year you were born:")
+
+print("Your were born on the year:", end_year)
+
+age = int(start_year) - int(end_year)
+
+print("Your age is:", age)
